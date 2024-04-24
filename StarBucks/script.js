67b666cfde24bb5488cad9ff25637f29069f39c8
Thejas <teju270934@gmail.com>
2024-04-24 10:35:16 +0530

    simple starbuks landing page , with html,css and js

---
+++ StarBucks/script.js
@@ -0,0 +1,41 @@
+'use strict ';
+
+// only modal window concept applied here 
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.modal-button');
+const openModal = document.querySelector('.menu-btns');
+
+const opnModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const clsModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+openModal.addEventListener('click', opnModal);
+
+closeModal.addEventListener('click', clsModal);
+overlay.addEventListener('click', clsModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) clsModal();
+});
+
+//
+const learnMore = document.querySelector('.btns');
+
+learnMore.addEventListener('click', opnModal);
+
+
+// for next page button 
+
+const nextPageBtn = document.querySelector('#next-page-btn');
+
+nextPageBtn.addEventListener('click',function(){
+nextPageBtn.classList.add('hidden-2');
+nextPageBtn.classList.remove('hidden-2');
+});
